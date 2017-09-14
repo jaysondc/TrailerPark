f34457fe1c5cf2fd3b58974656fb11fe31478851
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2017-09-14 13:17:51 -0700

    Added object models for movies and trailers.

---
+++ app/src/main/java/com/shakeup/trailerpark/commons/Models.kt
@@ -0,0 +1,59 @@
+package com.shakeup.trailerpark.commons;
+
+/**
+ * Created by Jayson on 9/14/2017.
+ *
+ * Holds the model objects for Movie and Trailer items
+ */
+
+/**
+ * Object returned by the Now Playing endpoint
+ */
+data class NowPlaying(
+        val results: List<MovieItem>,
+        val page: Int,
+        val total_results: Int,
+        // val dates: DateRange,
+        val total_pages: Int
+)
+
+/**
+ * Object for individual movies
+ */
+data class MovieItem(
+        val vote_count: Int,
+        val id: Int,
+        val video: Boolean,
+        val vote_average: Double,
+        val title: String,
+        val popularity: Float,
+        val poster_path: String,
+        val original_language: String,
+        val original_title: String,
+        val genre_ids: List<Int>,
+        val backdrop_path: String,
+        val adult: Boolean,
+        val overview: String,
+        val release_date: String
+)
+
+data class Trailers(
+        val id: Int,
+        val results: List<TrailerItem>
+)
+
+/**
+ * Object for individual Trailers
+ */
+data class TrailerItem(
+        val id: Int,
+        val iso_639_1: String,
+        val iso_3166_1: String,
+        val key: String,
+        val name: String,
+        val site: String,
+        val size: Int,
+        val type: String
+)
+
+
